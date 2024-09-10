056164d1a0fd96e209efffddb7b19a2ce4b69b07
Thomas Blumenfeld <thomas.blumenfeld@gmail.com>
2024-09-10 18:54:51 +0200

    added ruby task api

---
+++ task_api/app/controllers/tasks_controller.rb
@@ -0,0 +1,50 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :update, :destroy]
+
+  # GET /tasks
+  def index
+    @tasks = Task.all
+    render json: @tasks
+  end
+
+  # GET /tasks/:id
+  def show
+    render json: @task
+  end
+
+  # POST /tasks
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      render json: @task, status: :created
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /tasks/:id
+  def update
+    if @task.update(task_params)
+      render json: @task
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /tasks/:id
+  def destroy
+    @task.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:text, :done)
+  end
+end
